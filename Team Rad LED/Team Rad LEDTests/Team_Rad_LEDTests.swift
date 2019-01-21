9b98f029d02ff2f714271030154a6fb4d8fd7a1b
Pascale Walters <pascalewalters@gmail.com>
2019-01-21 15:54:26 -0500

    added bluetooth demo app

---
+++ Team Rad LED/Team Rad LEDTests/Team_Rad_LEDTests.swift
@@ -0,0 +1,34 @@
+//
+//  Team_Rad_LEDTests.swift
+//  Team Rad LEDTests
+//
+//  Created by Pascale Walters on 2018-11-06.
+//  Copyright © 2018 Pascale Walters. All rights reserved.
+//
+
+import XCTest
+@testable import Team_Rad_LED
+
+class Team_Rad_LEDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
