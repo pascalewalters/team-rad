bfc3281634e5c7b27791046f84a80096afded5c8
Pascale Walters <pascalewalters@gmail.com>
2019-01-30 16:26:17 -0500

    first commit

--- MultiobjectTracker/ObjectTracker/ViewController.swift
+++ MultiobjectTracker/ObjectTracker/ViewController.swift
@@ -33,8 +33,72 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     private var lastObservation: [UUID: VNDetectedObjectObservation] = [:]
     private var trackingRequests: [VNTrackObjectRequest] = []
     
+    let yolo = YOLO()
+    
+    // How many predictions we can do concurrently.
+    static let maxInflightBuffers = 3
+    
+    let semaphore = DispatchSemaphore(value: ViewController.maxInflightBuffers)
+
+    var boundingBoxes = [BoundingBox]()
+    var colors: [UIColor] = []
+    var resizedPixelBuffers: [CVPixelBuffer?] = []
+    var requests = [VNCoreMLRequest]()
+    var startTimes: [CFTimeInterval] = []
+    var framesDone = 0
+    var frameCapturingStartTime = CACurrentMediaTime()
+    var inflightBuffer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set up bounding boxes
+        for _ in 0..<YOLO.maxBoundingBoxes {
+            boundingBoxes.append(BoundingBox())
+        }
+        
+        // Make colors for the bounding boxes. There is one color for each class,
+        // 20 classes in total.
+        for r: CGFloat in [0.2, 0.4, 0.6, 0.8, 1.0] {
+            for g: CGFloat in [0.3, 0.7] {
+                for b: CGFloat in [0.4, 0.8] {
+                    let color = UIColor(red: r, green: g, blue: b, alpha: 1)
+                    colors.append(color)
+                }
+            }
+        }
+        
+        // set up core image
+        // Since we might be running several requests in parallel, we also need
+        // to do the resizing in different pixel buffers or we might overwrite a
+        // pixel buffer that's already in use.
+        for _ in 0..<YOLO.maxBoundingBoxes {
+            var resizedPixelBuffer: CVPixelBuffer?
+            let status = CVPixelBufferCreate(nil, YOLO.inputWidth, YOLO.inputHeight,
+                                             kCVPixelFormatType_32BGRA, nil,
+                                             &resizedPixelBuffer)
+            
+            if status != kCVReturnSuccess {
+                print("Error: could not create resized pixel buffer", status)
+            }
+            resizedPixelBuffers.append(resizedPixelBuffer)
+        }
+        
+        // set up vision
+        guard let visionModel = try? VNCoreMLModel(for: yolo.model.model) else {
+            print("Error: could not create Vision model")
+            return
+        }
+        
+        for _ in 0..<ViewController.maxInflightBuffers {
+            let request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            
+            // NOTE: If you choose another crop/scale option, then you must also
+            // change how the BoundingBox objects get scaled when they are drawn.
+            // Currently they assume the full input image is used.
+            request.imageCropAndScaleOption = .scaleFill
+            requests.append(request)
+        }
         
         // make the camera appear on the screen
         self.cameraView?.layer.addSublayer(self.cameraLayer)
@@ -48,11 +112,123 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         self.captureSession.startRunning()
     }
     
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        if let observations = request.results as? [VNCoreMLFeatureValueObservation],
+            let features = observations.first?.featureValue.multiArrayValue {
+            
+            let boundingBoxes = yolo.computeBoundingBoxes(features: features)
+            let elapsed = CACurrentMediaTime() - startTimes.remove(at: 0)
+            // TODO: For each bounding box, create a new observation
+            // boundingBoxes are in YOLO coordinates
+            showOnMainThread(boundingBoxes, elapsed)
+        } else {
+            print("BOGUS!")
+        }
+        
+        self.semaphore.signal()
+    }
+    
+    func showOnMainThread(_ boundingBoxes: [YOLO.Prediction], _ elapsed: CFTimeInterval) {
+        DispatchQueue.main.async {
+            // For debugging, to make sure the resized CVPixelBuffer is correct.
+            //var debugImage: CGImage?
+            //VTCreateCGImageFromCVPixelBuffer(resizedPixelBuffer, nil, &debugImage)
+            //self.debugImageView.image = UIImage(cgImage: debugImage!)
+            //        if boundingBoxes.count < 1 { return }
+            self.show(predictions: boundingBoxes)
+            
+//            print(self.measureFPS())
+            // Don't have this IBOutlet
+//            self.timeLabel.text = String(format: "Elapsed %.5f seconds - %.2f FPS", elapsed, fps)
+        }
+    }
+    
+    func measureFPS() -> Double {
+        // Measure how many frames were actually delivered per second.
+        framesDone += 1
+        let frameCapturingElapsed = CACurrentMediaTime() - frameCapturingStartTime
+        let currentFPSDelivered = Double(framesDone) / frameCapturingElapsed
+        if frameCapturingElapsed > 1 {
+            framesDone = 0
+            frameCapturingStartTime = CACurrentMediaTime()
+        }
+        return currentFPSDelivered
+    }
+    
+    func show(predictions: [YOLO.Prediction]) {
+        for i in 0..<boundingBoxes.count {
+            if i < predictions.count {
+                let prediction = predictions[i]
+                print(labels[prediction.classIndex])
+                print(prediction.rect)
+//                if labels[prediction.classIndex] != "bicycle" && labels[prediction.classIndex] != "bus" && labels[prediction.classIndex] != "car" && labels[prediction.classIndex] != "motorbike" { continue }
+                
+                // The predicted bounding box is in the coordinate space of the input
+                // image, which is a square image of 416x416 pixels. We want to show it
+                // on the video preview, which is as wide as the screen and has a 16:9
+                // aspect ratio. The video preview also may be letterboxed at the top
+                // and bottom.
+                let width = view.bounds.width
+                let height = width * 16 / 9
+                let scaleX = width / CGFloat(YOLO.inputWidth)
+                let scaleY = height / CGFloat(YOLO.inputHeight)
+                let top = (view.bounds.height - height) / 2
+                
+                // Translate and scale the rectangle to our own coordinate system.
+                var rect = prediction.rect
+                rect.origin.x *= scaleX
+                rect.origin.y *= scaleY
+                rect.origin.y += top
+                rect.size.width *= scaleX
+                rect.size.height *= scaleY
+                
+                print(rect)
+                
+                // rect is in preview layer coords
+                
+                var convertedRect = self.cameraLayer.metadataOutputRectConverted(fromLayerRect: rect)
+                convertedRect.origin.y = 1 - convertedRect.origin.y
+                
+                let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect)
+                lastObservation[newObservation.uuid] = newObservation
+                
+                print(newObservation.boundingBox)
+                
+                // Show the bounding box.
+                let label = String(format: "%@ %.1f", labels[prediction.classIndex], prediction.score * 100)
+                let color = colors[prediction.classIndex]
+                boundingBoxes[i].show(frame: rect, label: label, color: color)
+                
+                
+            } else {
+                boundingBoxes[i].hide()
+            }
+        }
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         // make sure the layer is the correct size
         self.cameraLayer.frame = self.cameraView?.bounds ?? .zero
+    }
+    
+    func predictUsingVision(pixelBuffer: CVPixelBuffer, inflightIndex: Int) {
+        // Measure how long it takes to predict a single video frame. Note that
+        // predict() can be called on the next frame while the previous one is
+        // still being processed. Hence the need to queue up the start times.
+        startTimes.append(CACurrentMediaTime())
+        
+        // Vision will automatically resize the input image.
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        let request = requests[inflightIndex]
+        
+        // Because perform() will block until after the request completes, we
+        // run it on a concurrent background queue, so that the next frame can
+        // be scheduled in parallel with this one.
+        DispatchQueue.global().async {
+            try? handler.perform([request])
+        }
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -77,6 +253,21 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         } catch {
             print("Throws: \(error)")
         }
+        
+        // The semaphore will block the capture queue and drop frames when
+        // Core ML can't keep up with the camera.
+        semaphore.wait()
+        
+        // For better throughput, we want to schedule multiple prediction requests
+        // in parallel. These need to be separate instances, and inflightBuffer is
+        // the index of the current request.
+        let inflightIndex = inflightBuffer
+        inflightBuffer += 1
+        if inflightBuffer >= ViewController.maxInflightBuffers {
+            inflightBuffer = 0
+        }
+        
+        self.predictUsingVision(pixelBuffer: pixelBuffer, inflightIndex: inflightIndex)
     }
     
     private func handleVisionRequestUpdate(_ request: VNRequest, error: Error?) {
@@ -87,7 +278,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             guard let results = request.results as? [VNObservation] else { return }
             guard let observation = results.first as? VNDetectedObjectObservation else { return }
             
-            // From https://developer.apple.com/documentation/vision/tracking_multiple_objects_or_rectangles_in_video
             self.lastObservation[observation.uuid] = observation
             
             // check the confidence level before updating the UI
@@ -114,22 +304,25 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         }
     }
     
-    @IBAction private func userTapped(_ sender: UITapGestureRecognizer) {
-        // get the center of the tap
-        let highlightView = createHighlightSquare()
-        highlightView.center = sender.location(in: self.view)
-        
-        // convert the rect for the initial observation
-        let originalRect = highlightView.frame
-        var convertedRect = self.cameraLayer.metadataOutputRectConverted(fromLayerRect: originalRect)
-        convertedRect.origin.y = 1 - convertedRect.origin.y
-        
-        // set the observation
-        let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect)
-        self.lastObservation[newObservation.uuid] = newObservation
-        self.highlightViews[newObservation.uuid] = highlightView
-        self.view.addSubview(highlightView)
-    }
+//    @IBAction private func userTapped(_ sender: UITapGestureRecognizer) {
+//        // get the center of the tap
+//        let highlightView = createHighlightSquare()
+//        highlightView.center = sender.location(in: self.view)
+//
+//        // convert the rect for the initial observation
+//        let originalRect = highlightView.frame
+//        print(originalRect.origin)
+//        print(originalRect)
+//        var convertedRect = self.cameraLayer.metadataOutputRectConverted(fromLayerRect: originalRect)
+//        convertedRect.origin.y = 1 - convertedRect.origin.y
+//        print(convertedRect)
+//
+//        // set the observation
+//        let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect)
+//        self.lastObservation[newObservation.uuid] = newObservation
+//        self.highlightViews[newObservation.uuid] = highlightView
+//        self.view.addSubview(highlightView)
+//    }
     
     @IBAction private func resetTapped(_ sender: UIBarButtonItem) {
         self.lastObservation = [:]
